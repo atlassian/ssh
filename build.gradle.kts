6da907ad2e514a22f544b6ae0b27f8a797d02bee
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2018-08-30 11:48:30 +0200

    JPERF-32: Resolve all dependency conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,30 @@ plugins {
     id("com.atlassian.performance.tools.gradle-release").version("0.3.0")
 }
 
+configurations.all {
+    resolutionStrategy.failOnVersionConflict()
+    resolutionStrategy.eachDependency {
+        when (requested.module.toString()) {
+            "org.slf4j:slf4j-api" -> useVersion("1.8.0-alpha2")
+        }
+    }
+}
+
 dependencies {
     compile("com.atlassian.performance.tools:jvm-tasks:[1.0.0,2.0.0)")
     compile("com.atlassian.performance.tools:io:[1.0.0,2.0.0)")
     compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
     compile("org.glassfish:javax.json:1.1")
     compile("com.hierynomus:sshj:0.23.0")
-    log4j(
+    listOf(
         "api",
         "core",
         "slf4j-impl"
-    ).forEach { compile(it) }
+    ).forEach { compile("org.apache.logging.log4j:log4j-$it:2.10.0") }
     testCompile("junit:junit:4.12")
 }
 
-fun log4j(
-    vararg modules: String
-): List<String> = modules.map { module ->
-    "org.apache.logging.log4j:log4j-$module:2.10.0"
+task<Wrapper>("wrapper") {
+    gradleVersion = "4.9"
+    distributionType = Wrapper.DistributionType.ALL
 }
-
-val wrapper = tasks["wrapper"] as Wrapper
-wrapper.gradleVersion = "4.9"
-wrapper.distributionType = Wrapper.DistributionType.ALL
