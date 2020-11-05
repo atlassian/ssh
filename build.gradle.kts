3df5ab453b2c4b885bf853d4865ed95d6bc70256
Mikolaj Zyromski <mzyromski@atlassian.com>
2020-11-05 14:00:43 +0100

    JPERF-693: Bump gradle-release to fix release

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val kotlinVersion = "1.2.70"
 
 plugins {
     kotlin("jvm").version("1.2.70")
-    id("com.atlassian.performance.tools.gradle-release").version("0.5.0")
+    id("com.atlassian.performance.tools.gradle-release").version("0.7.1")
 }
 
 configurations.all {
